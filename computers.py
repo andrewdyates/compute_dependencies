e56e2f9479d9ebfe2dc4ed611c980d744862bc20
Andrew Yates <ayates@master.cl.osumc.edu>
2013-05-30 18:03:57 -0400

    make import of specialized method computers dynamic

--- computers.py
+++ computers.py
@@ -1,10 +1,41 @@
-import dcor_computer
-import hhg_computer
-import mine_computer
-import numpy_computers
+# these extra, custom modules should be imported on demand, not by default
 
-COMPUTERS = {}
-COMPUTERS.update(dcor_computer.COMPUTERS)
-COMPUTERS.update(hhg_computer.COMPUTERS)
-COMPUTERS.update(mine_computer.COMPUTERS)
-COMPUTERS.update(numpy_computers.COMPUTERS)
+# Create a COMPUTERS singleton class instance that is backwards compadiable with the 
+#   old dictionary based version
+class COMPUTERCLS(object):
+  # I don't have a great way to maintain this list automatically.
+  # Just add new names to the list as they are created.
+  # Map to module name
+  KNOWN_COMPUTERS = {
+    "Dcor", 'dcor_computer',
+    "PCC", 'numpy_computers',
+    "Cov", 'numpy_computers',
+    "Spearman", 'numpy_computers',
+    "Euclidean": 'numpy_computers',
+    "Kendall": 'numpy_computers',
+    "MINE": 'mine_computer',
+    "HHG": 'hhg_computer',
+  }
+  def keys(self):
+    return set(self.KNOWN_COMPUTERS.keys())
+  def __getitem__(self, arg):
+    # Get computer class from module, but only import on request
+    assert arg in self.KNOWN_COMPUTERS
+    mod = self.KNOWN_COMPUTERS[arg]
+    globals()[mod] = __import__("mod")
+    return globals()[mod].COMPUTERS[arg]
+
+COMPUTERS = COMPUTERCLS()
+
+# OLD WAY
+# -----------------------------
+# import dcor_computer
+# import hhg_computer
+# import mine_computer
+# import numpy_computers
+
+# COMPUTERS = {}
+# COMPUTERS.update(dcor_computer.COMPUTERS)
+# COMPUTERS.update(hhg_computer.COMPUTERS)
+# COMPUTERS.update(mine_computer.COMPUTERS)
+# COMPUTERS.update(numpy_computers.COMPUTERS)
